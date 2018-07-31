polarssl/polarssl

Reduce the number of Keccak/SHA-3/SHAKE error codes
All the BAD_STATE errors come from Keccak. They don't need to be
converted to a SHA-3/SHAKE code.

Unify NOT_SETUP into BAD_STATE since it fits semantically.

include/mbedtls/keccak.h
@@ -32,8 +32,7 @@
 #endif
 
 #define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA -0x0054 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_NOT_SETUP      -0x0056 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x0058 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x0056 /**< Requested operation cannot be performed with the current context state. */
 
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BITS  ( 1600U )
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BYTES ( 1600U / 8U )
@@ -177,7 +176,7 @@ void mbedtls_keccak_sponge_clone( mbedtls_keccak_sponge_context *dst,
  *                      mbedtls_keccak_sponge_init and before calling the
  *                      absorb or squeeze functions. If this function has not
  *                      been called then the absorb/squeeze functions will
- *                      return MBEDTLS_ERR_KECCAK_NOT_SETUP.
+ *                      return MBEDTLS_ERR_KECCAK_BAD_STATE.
  *
  * \param ctx           The sponge context to setup.
  * \param capacity      The sponge's capacity parameter. This determines the
@@ -219,7 +218,7 @@ int mbedtls_keccak_sponge_starts( mbedtls_keccak_sponge_context *ctx,
  *                      sponge can no longer accept data for absorption. This
  *                      occurs when mbedtls_keccak_sponge_squeeze has been previously
  *                      called.
- *                      MBEDTLS_ERR_KECCAK_NOT_SETUP is returned if
+ *                      MBEDTLS_ERR_KECCAK_BAD_STATE is returned if
  *                      mbedtls_keccak_sponge_starts has not yet been called to
  *                      configure the context.
  *                      Otherwise, 0 is returned to indicate success.
@@ -243,7 +242,7 @@ int mbedtls_keccak_sponge_absorb( mbedtls_keccak_sponge_context *ctx,
  *
  * \return              MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA is returned if
  *                      ctx or data is NULL.
- *                      MBEDTLS_ERR_KECCAK_NOT_SETUP is returned if
+ *                      MBEDTLS_ERR_KECCAK_BAD_STATE is returned if
  *                      mbedtls_keccak_sponge_starts has not yet been called to
  *                      configure the context.
  *                      Otherwise, 0 is returned to indicate success.

include/mbedtls/sha3.h
@@ -40,9 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0055 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x0057 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x0059 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0053 /**< Invalid input parameter(s). */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -40,9 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x005B /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x005D /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x005F /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x0057 /**< Invalid input parameter(s). */
 
 typedef enum
 {

gilles-peskine-arm/mbedtls

include/mbedtls/keccak.h
@@ -32,8 +32,7 @@
 #endif
 
 #define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA -0x001B /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_NOT_SETUP      -0x001D /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x001F /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x001D /**< Requested operation cannot be performed with the current context state. */
 
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BITS  ( 1600U )
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BYTES ( 1600U / 8U )
@@ -177,7 +176,7 @@ void mbedtls_keccak_sponge_clone( mbedtls_keccak_sponge_context *dst,
  *                      mbedtls_keccak_sponge_init and before calling the
  *                      absorb or squeeze functions. If this function has not
  *                      been called then the absorb/squeeze functions will
- *                      return MBEDTLS_ERR_KECCAK_NOT_SETUP.
+ *                      return MBEDTLS_ERR_KECCAK_BAD_STATE.
  *
  * \param ctx           The sponge context to setup.
  * \param capacity      The sponge's capacity parameter. This determines the
@@ -219,7 +218,7 @@ int mbedtls_keccak_sponge_starts( mbedtls_keccak_sponge_context *ctx,
  *                      sponge can no longer accept data for absorption. This
  *                      occurs when mbedtls_keccak_sponge_squeeze has been previously
  *                      called.
- *                      MBEDTLS_ERR_KECCAK_NOT_SETUP is returned if
+ *                      MBEDTLS_ERR_KECCAK_BAD_STATE is returned if
  *                      mbedtls_keccak_sponge_starts has not yet been called to
  *                      configure the context.
  *                      Otherwise, 0 is returned to indicate success.
@@ -243,7 +242,7 @@ int mbedtls_keccak_sponge_absorb( mbedtls_keccak_sponge_context *ctx,
  *
  * \return              MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA is returned if
  *                      ctx or data is NULL.
- *                      MBEDTLS_ERR_KECCAK_NOT_SETUP is returned if
+ *                      MBEDTLS_ERR_KECCAK_BAD_STATE is returned if
  *                      mbedtls_keccak_sponge_starts has not yet been called to
  *                      configure the context.
  *                      Otherwise, 0 is returned to indicate success.

include/mbedtls/sha3.h
@@ -40,9 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0061 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHA3_BAD_NOT_STARTED  -0x0063 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHA3_BAD_STATE        -0x0065 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x003B /**< Invalid input parameter(s). */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -40,9 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x0067 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x0069 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x006B /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x001F /**< Invalid input parameter(s). */
 
 typedef enum
 {
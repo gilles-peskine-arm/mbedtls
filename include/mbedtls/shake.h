72220cd5b7567050fa94bfa757b7876552cb94df
Gilles Peskine <Gilles.Peskine@arm.com>
2018-03-30 20:58:28 +0200

    Reduce the number of Keccak/SHA-3/SHAKE error codes
    All the BAD_STATE errors come from Keccak. They don't need to be
    converted to a SHA-3/SHAKE code.
    
    Unify NOT_SETUP into BAD_STATE since it fits semantically.

--- include/mbedtls/shake.h
+++ include/mbedtls/shake.h
@@ -40,9 +40,7 @@
 extern "C" {
 #endif
 
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x0067 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_SHAKE_BAD_NOT_STARTED  -0x0069 /**< mbedtls_keccak_sponge_starts has not been called. */
-#define MBEDTLS_ERR_SHAKE_BAD_STATE        -0x006B /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x001F /**< Invalid input parameter(s). */
 
 typedef enum
 {
