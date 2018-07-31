polarssl/polarssl

Add HW_FAILED error codes for Keccak/SHA-3/SHAKE
Also define the error codes in the part of the header file that isn't
under the !_ALT conditional directive.

include/mbedtls/keccak.h
@@ -31,8 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-#define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA -0x0054 /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x0056 /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA                 -0x0054  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_KECCAK_BAD_STATE                      -0x0056  /**< The requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_HW_ACCEL_FAILED                -0x0058  /**< SHAKE hardware accelerator failed. */
 
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BITS  ( 1600U )
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BYTES ( 1600U / 8U )

include/mbedtls/sha3.h
@@ -31,6 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA                   -0x0053  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_SHA3_HW_ACCEL_FAILED                  -0x0055  /**< SHA3 hardware accelerator failed. */
+
 #if !defined(MBEDTLS_SHA3_ALT)
 // Regular implementation
 
@@ -39,8 +42,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x0053 /**< Invalid input parameter(s). */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -31,6 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA                  -0x0057  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_SHAKE_HW_ACCEL_FAILED                 -0x0059  /**< SHAKE hardware accelerator failed. */
+
 #if !defined(MBEDTLS_SHAKE_ALT)
 // Regular implementation
 
@@ -39,8 +42,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x0057 /**< Invalid input parameter(s). */
 
 typedef enum
 {

gilles-peskine-arm/mbedtls

include/mbedtls/keccak.h
@@ -31,8 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
-#define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA -0x001B /**< Invalid input parameter(s). */
-#define MBEDTLS_ERR_KECCAK_BAD_STATE      -0x001D /**< Requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_BAD_INPUT_DATA                 -0x001B  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_KECCAK_BAD_STATE                      -0x001D  /**< The requested operation cannot be performed with the current context state. */
+#define MBEDTLS_ERR_KECCAK_HW_ACCEL_FAILED                -0x001F  /**< SHAKE hardware accelerator failed. */
 
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BITS  ( 1600U )
 #define MBEDTLS_KECCAK_F_STATE_SIZE_BYTES ( 1600U / 8U )

include/mbedtls/sha3.h
@@ -31,6 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
+#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA                   -0x0061  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_SHA3_HW_ACCEL_FAILED                  -0x0063  /**< SHA3 hardware accelerator failed. */
+
 #if !defined(MBEDTLS_SHA3_ALT)
 // Regular implementation
 
@@ -39,8 +42,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define MBEDTLS_ERR_SHA3_BAD_INPUT_DATA   -0x003B /**< Invalid input parameter(s). */
 
 typedef enum
 {

include/mbedtls/shake.h
@@ -31,6 +31,9 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
+#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA                  -0x0065  /**< Bad input parameters to function. */
+#define MBEDTLS_ERR_SHAKE_HW_ACCEL_FAILED                 -0x0067  /**< SHAKE hardware accelerator failed. */
+
 #if !defined(MBEDTLS_SHAKE_ALT)
 // Regular implementation
 
@@ -39,8 +42,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define MBEDTLS_ERR_SHAKE_BAD_INPUT_DATA   -0x001F /**< Invalid input parameter(s). */
 
 typedef enum
 {