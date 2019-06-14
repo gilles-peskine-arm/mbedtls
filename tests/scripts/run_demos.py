427c73327d3d4234a6ee5ffdb93a2194571797b2
Gilles Peskine <Gilles.Peskine@arm.com>
2019-06-14 18:23:03 +0200

    Run demo scripts and check that they work
    run_demos.py is the frontend to a framework for smoke-testing the
    sample programs. It runs scripts called programs/*/*_demo.sh
    ("demo scripts") and check that they succeed. A typical demo script
    runs one sample program or a combination of sample programs to
    demonstrate their usage.

---
+++ tests/scripts/run_demos.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Run the Mbed TLS demo scripts.
+"""
+import glob
+import re
+import subprocess
+
+def config_has(symbol):
+    return subprocess.call(['scripts/config.pl', 'get', symbol]) == 0
+
+def is_demo_applicable(demo):
+    """Return True if the specified demo is applicable in the current configuration."""
+    # For the time being, just do an ad hoc detection of demos that need PSA.
+    # This should be made more principled when we add more demos.
+    if re.search(r'[/_]psa[/_]', demo):
+        return config_has('MBEDTLS_PSA_CRYPTO_C')
+    return True
+
+def run_demo(demo):
+    """Run the specified demo script. Return True if it succeeds."""
+    returncode = subprocess.call([demo])
+    return returncode == 0
+
+def run_demos(demos):
+    """Run the specified demos and print summary information about failures.
+
+Return True if all demos passed and False if a demo fails."""
+    failures = []
+    for demo in demos:
+        print('#### {} ####'.format(demo))
+        if not run_demo(demo):
+            failures.append(demo)
+            print('{}: FAIL'.format(demo))
+        print('')
+    successes = len(demos) - len(failures)
+    print('{}/{} demos passed'.format(successes, len(demos)))
+    if failures:
+        print('Failures:', *failures)
+    return not failures
+
+def run_all_demos():
+    """Run all the available demos that are application in the current configuration.
+
+Return True if all demos passed and False if a demo fails."""
+    all_demos = glob.glob('programs/*/*_demo.sh')
+    applicable_demos = [demo for demo in all_demos if is_demo_applicable(demo)]
+    return run_demos(applicable_demos)
+
+if __name__ == '__main__':
+    if not run_all_demos():
+        sys.exit(1)
