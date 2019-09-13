1f486e759b4e6a81bdd6a180e5e7552de4e4d620
Gilles Peskine <Gilles.Peskine@arm.com>
2019-09-13 17:22:54 +0200

    Base64: replace lookup tables by code
    Portability: the code now requires the encodings of uppercase letters
    and lowercase letters to each form a consecutive block.
    
    Speed: depends on the speed of code execution vs table lookup.
    
    Security: no impact.
    
    Readability: I think this is a slight improvement but
    mbedtls_base64_decode could use work.

--- library/base64.c
+++ library/base64.c
@@ -66,33 +66,23 @@
 #endif /* MBEDTLS_PLATFORM_C */
 #endif /* MBEDTLS_SELF_TEST */
 
-static const unsigned char base64_enc_map[64] =
+/* Given a value in the range 0..63, return the corresponding base64 digit.
+ * The implementation assumes that letters are consecutive (e.g. ASCII
+ * but not EBCDIC).
+ */
+static unsigned char base64_enc_char( unsigned char value )
 {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', '+', '/'
-};
-
-static const unsigned char base64_dec_map[128] =
-{
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
-    127, 127, 127,  62, 127, 127, 127,  63,  52,  53,
-     54,  55,  56,  57,  58,  59,  60,  61, 127, 127,
-    127,  64, 127, 127, 127,   0,   1,   2,   3,   4,
-      5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
-     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
-     25, 127, 127, 127, 127, 127, 127,  26,  27,  28,
-     29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
-     39,  40,  41,  42,  43,  44,  45,  46,  47,  48,
-     49,  50,  51, 127, 127, 127, 127, 127
-};
+    if( value < 26 )
+        return( 'A' + value );
+    else if( value < 52 )
+        return( 'a' + value - 26 );
+    else if( value < 62 )
+        return( '0' + value - 52 );
+    else if( value == 62 )
+        return( '+' );
+    else
+        return( '/' );
+}
 
 #define BASE64_SIZE_T_MAX   ( (size_t) -1 ) /* SIZE_T_MAX is not standard */
 
@@ -136,10 +126,10 @@ int mbedtls_base64_encode( unsigned char *dst, size_t dlen, size_t *olen,
         C2 = *src++;
         C3 = *src++;
 
-        *p++ = base64_enc_map[(C1 >> 2) & 0x3F];
-        *p++ = base64_enc_map[(((C1 &  3) << 4) + (C2 >> 4)) & 0x3F];
-        *p++ = base64_enc_map[(((C2 & 15) << 2) + (C3 >> 6)) & 0x3F];
-        *p++ = base64_enc_map[C3 & 0x3F];
+        *p++ = base64_enc_char( (C1 >> 2) & 0x3F );
+        *p++ = base64_enc_char( (((C1 &  3) << 4) + (C2 >> 4)) & 0x3F );
+        *p++ = base64_enc_char( (((C2 & 15) << 2) + (C3 >> 6)) & 0x3F );
+        *p++ = base64_enc_char( C3 & 0x3F );
     }
 
     if( i < slen )
@@ -147,11 +137,11 @@ int mbedtls_base64_encode( unsigned char *dst, size_t dlen, size_t *olen,
         C1 = *src++;
         C2 = ( ( i + 1 ) < slen ) ? *src++ : 0;
 
-        *p++ = base64_enc_map[(C1 >> 2) & 0x3F];
-        *p++ = base64_enc_map[(((C1 & 3) << 4) + (C2 >> 4)) & 0x3F];
+        *p++ = base64_enc_char( (C1 >> 2) & 0x3F );
+        *p++ = base64_enc_char( (((C1 & 3) << 4) + (C2 >> 4)) & 0x3F );
 
         if( ( i + 1 ) < slen )
-             *p++ = base64_enc_map[((C2 & 15) << 2) & 0x3F];
+            *p++ = base64_enc_char( ((C2 & 15) << 2) & 0x3F );
         else *p++ = '=';
 
         *p++ = '=';
@@ -161,6 +151,59 @@ int mbedtls_base64_encode( unsigned char *dst, size_t dlen, size_t *olen,
     *p = 0;
 
     return( 0 );
+}
+
+typedef enum
+{
+    CR,
+    LF,
+    SPACE,
+    EQUAL,
+    BODY,
+    INVALID
+} base64_character_type;
+
+/* Given any byte value, return its syntactic category in Base64 source.
+ * The implementation assumes that letters are consecutive (e.g. ASCII
+ * but not EBCDIC).
+ */
+static base64_character_type base64_get_type( unsigned char c )
+{
+    switch( c )
+    {
+        case '\r': return( CR );
+        case '\n': return( LF );
+        case ' ': return( SPACE );
+        case '=': return( EQUAL );
+        case '+': return( BODY );
+        case '/': return( BODY );
+        default:
+            if( '0' <= c && c <= '9' )
+                return( BODY );
+            if( 'A' <= c && c <= 'Z' )
+                return( BODY );
+            if( 'a' <= c && c <= 'z' )
+                return( BODY );
+            return( INVALID );
+    }
+}
+
+/* Given a Base64 digit, return its value.
+ * The implementation assumes that letters are consecutive (e.g. ASCII
+ * but not EBCDIC).
+ */
+static unsigned char base64_dec_char( unsigned char c )
+{
+    if( 'A' <= c && c <= 'Z' )
+        return( c - 'A' );
+    else if( '0' <= c && c <= '9' )
+        return( c - '0' + 52 );
+    else if( 'a' <= c && c <= 'z' )
+        return( c - 'a' + 26 );
+    else if( c == '+' )
+        return( 62 );
+    else
+        return( 63 );
 }
 
 /*
@@ -176,36 +219,43 @@ int mbedtls_base64_decode( unsigned char *dst, size_t dlen, size_t *olen,
     /* First pass: check for validity and get output length */
     for( i = n = j = 0; i < slen; i++ )
     {
+        base64_character_type type = base64_get_type( src[i] );
+
         /* Skip spaces before checking for EOL */
         x = 0;
-        while( i < slen && src[i] == ' ' )
+        while( i < slen && type == SPACE )
         {
             ++i;
             ++x;
+            type = base64_get_type( src[i] );
         }
 
         /* Spaces at end of buffer are OK */
         if( i == slen )
             break;
 
-        if( ( slen - i ) >= 2 &&
-            src[i] == '\r' && src[i + 1] == '\n' )
-            continue;
+        if( type == CR )
+        {
+            if( slen - i >= 2 && base64_get_type( src[i + 1] ) == LF )
+                continue;
+            else
+                return( MBEDTLS_ERR_BASE64_INVALID_CHARACTER );
+        }
 
-        if( src[i] == '\n' )
+        if( type == LF )
             continue;
 
         /* Space inside a line is an error */
         if( x != 0 )
             return( MBEDTLS_ERR_BASE64_INVALID_CHARACTER );
 
-        if( src[i] == '=' && ++j > 2 )
+        if( type == EQUAL && ++j > 2 )
             return( MBEDTLS_ERR_BASE64_INVALID_CHARACTER );
 
-        if( src[i] > 127 || base64_dec_map[src[i]] == 127 )
+        if( type == INVALID )
             return( MBEDTLS_ERR_BASE64_INVALID_CHARACTER );
 
-        if( base64_dec_map[src[i]] < 64 && j != 0 )
+        if( type == BODY && j != 0 )
             return( MBEDTLS_ERR_BASE64_INVALID_CHARACTER );
 
         n++;
@@ -232,11 +282,19 @@ int mbedtls_base64_decode( unsigned char *dst, size_t dlen, size_t *olen,
 
    for( j = 3, n = x = 0, p = dst; i > 0; i--, src++ )
    {
-        if( *src == '\r' || *src == '\n' || *src == ' ' )
-            continue;
-
-        j -= ( base64_dec_map[*src] == 64 );
-        x  = ( x << 6 ) | ( base64_dec_map[*src] & 0x3F );
+        base64_character_type type = base64_get_type( *src );
+        switch( type )
+        {
+            case BODY:
+                x = ( x << 6 ) | base64_dec_char( *src );
+                break;
+            case EQUAL:
+                j -= 1;
+                x <<= 6;
+                break;
+            default:
+                continue;
+        }
 
         if( ++n == 4 )
         {
