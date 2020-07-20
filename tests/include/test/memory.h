a4fb240102437bff5de39a9939030dcc2d5a877f
Gilles Peskine <Gilles.Peskine@arm.com>
2020-07-20 09:15:11 +0200

    Framework for low-memory tests
    When the test suite is compiled with MBEDTLS_TEST_MEMORY_CALLOC_FAILURE
    defined, run each test case multiple times to see what happens if a memory
    allocation (call to mbedtls_calloc()) made by the library fails. First try
    failing the first call to mbedtls_calloc(), then the second one, etc. Stop
    when all calls have succeeded (i.e. a normal test run).
    
    Check for memory leaks (or outright crashes) in runs with a forced
    allocation. In this commit the reporting is crude and does not cause the
    test suite to exit with a failue status. Subsequent commits will fix these
    issues.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/include/test/memory.h
+++ tests/include/test/memory.h
@@ -33,6 +33,11 @@
 #include MBEDTLS_CONFIG_FILE
 #endif
 
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+/* Forced allocation failures are only possible with the memory wrappers. */
+#define MBEDTLS_TEST_MEMORY_WRAPPERS
+#endif
+
 #if defined(MBEDTLS_TEST_MEMORY_WRAPPERS)
 
 #include <stddef.h>
@@ -70,5 +75,48 @@ void mbedtls_test_memory_get_stats( size_t *allocations, size_t *bytes,
 #define mbedtls_test_memory_teardown( ) ( (void) 0 )
 
 #endif /* MBEDTLS_TEST_MEMORY_WRAPPERS */
+
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+#include <stdbool.h>
+
+/** Report whether there has been a forced memory allocation failure during
+ * the current run of the test.
+ *
+ * \return Whether there has been a forced failure during the current test
+ *         case run.
+ */
+bool mbedtls_test_memory_has_forced_failure( void );
+
+/** Return the position of the forced failure in the current run.
+ *
+ * \return N such that the Nth allocation during this test run was forced
+ *         to fail.
+ * \return \c -1 if no allocation was forced to fail during this test run.
+ */
+size_t mbedtls_test_memory_get_forced_failure( void );
+
+/** Prepare to run a test case again with the next location of forced
+ * allocation failure.
+ *
+ * Run test cases in a loop like this:
+ * ```
+ * mbedtls_test_memory_setup( );
+ * test_case( );
+ * while( mbedtls_test_memory_has_forced_failure( ) )
+ * {
+ *    mbedtls_test_memory_next_forced_failure( );
+ *    test_case( );
+ * }
+ * mbedtls_test_memory_teardown( );
+ * ```
+ *
+ * Each run will force a failure of a call to mbedtls_calloc() at
+ * successively later locations, with this function returning 1.
+ *  Eventually the point of failure will not be reached, and in that case
+ * this function returns 0.
+ */
+void mbedtls_test_memory_next_forced_failure( void );
+
+#endif /* MBEDTLS_TEST_MEMORY_CALLOC_FAILURE */
 
 #endif /* TEST_MEMORY_H */
--- tests/src/memory.c
+++ tests/src/memory.c
@@ -42,9 +42,26 @@ typedef struct
 } mbedtls_test_memory_stats_t;
 static mbedtls_test_memory_stats_t stats;
 
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+#include <stdbool.h>
+
+typedef struct
+{
+    size_t fail_n; /* Fail the Nth allocation (counting from 0) */
+    bool failed; /* A forced failure has happened */
+} mbedtls_test_memory_calloc_forced_failures_t;
+static mbedtls_test_memory_calloc_forced_failures_t forced_failures;
+#endif /* MBEDTLS_TEST_MEMORY_CALLOC_FAILURE */
+
 void mbedtls_test_memory_setup( void )
 {
     memset( &stats, 0, sizeof( stats ) );
+
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+    /* Set up the forced failure plan for the first run: fail the first
+     * allocation. See mbedtls_test_memory_next_forced_failure(). */
+    memset( &forced_failures, 0, sizeof( forced_failures ) );
+#endif /* MBEDTLS_TEST_MEMORY_CALLOC_FAILURE */
 }
 
 void mbedtls_test_memory_teardown( void )
@@ -66,8 +83,21 @@ void *mbedtls_test_calloc_wrapper( size_t n, size_t size )
      * function returns NULL, we know that the allocation has failed. */
     if( n == 0 || size == 0 )
         return( NULL );
+
+    void *ptr = NULL;
+
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+    if( stats.total_allocations == forced_failures.fail_n )
+    {
+        forced_failures.failed = 1;
+    }
+    else
+#endif
+    {
+        ptr = mbedtls_calloc( n, size );
+    }
+
     ++stats.total_allocations;
-    void *ptr = mbedtls_calloc( n, size );
     if( ptr == NULL )
     {
         ++stats.failed_allocations;
@@ -87,5 +117,42 @@ void mbedtls_test_free_wrapper( void *ptr )
     --stats.active_allocations;
     mbedtls_free( ptr );
 }
+
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_FAILURE)
+bool mbedtls_test_memory_has_forced_failure( void )
+{
+    return( forced_failures.failed );
+}
+
+size_t mbedtls_test_memory_get_forced_failure( void )
+{
+    if( forced_failures.failed )
+        return( forced_failures.fail_n );
+    else
+        return( -1 );
+}
+
+void mbedtls_test_memory_next_forced_failure( void )
+{
+    forced_failures.failed = 0;
+
+    /* Successive runs of the test care will fail the Nth allocation, for
+     * successive values of N. N starts at 0 (this is set in
+     * mbedtls_test_memory_setup()). Eventually N will be larger than
+     * the number of allocations that the test case performed, so the
+     * test case will run normally (with no forced allocation failure).
+     *
+     * We don't try multiple failures. Most test cases give up after the
+     * first allocation failure anyway.
+     *
+     * This works well only if the pattern of allocations performed by the
+     * test case is the same on every run. This may not be the case with
+     * randomized tests.
+     */
+    ++forced_failures.fail_n;
+
+    memset( &stats, 0, sizeof( stats ) );
+}
+#endif
 
 #endif /* MBEDTLS_TEST_MEMORY_WRAPPERS */
