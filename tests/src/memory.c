08ed08f649a81010f53ca86575a48d7c5bf5989b
Gilles Peskine <Gilles.Peskine@arm.com>
2020-07-23 22:45:56 +0200

    Implement memory allocation backtrace logs
    When the library is compiled with MBEDTLS_TEST_MEMORY_CALLOC_BACKTRACE
    in addition to MBEDTLS_TEST_MEMORY_WRAPPERS, print a backtrace to
    $MBEDTLS_TEST_MEMORY_CALLOC_LOG_FILE (defaulting to
    calloc_backtrace.log) each time the mbedtls_calloc() wrapper is called.
    
    The feature requires Glibc (or a library with a compatible
    <execinfo.h>). The library must be linked with `-rdynamic`.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/src/memory.c
+++ tests/src/memory.c
@@ -17,6 +17,10 @@
  *  This file is part of mbed TLS (https://tls.mbed.org)
  */
 
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_BACKTRACE)
+#define _POSIX_C_SOURCE 200112L // for fileno() from <stdio.h>
+#endif
+
 #include <test/helpers.h>
 #include <test/memory.h>
 #include <test/memory_wrappers.h>
@@ -65,8 +69,57 @@ void mbedtls_test_memory_get_total_stats( mbedtls_test_memory_stats_t *stats )
     update_stats( stats );
 }
 
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_BACKTRACE)
+#include <execinfo.h> // GLIBC extension, provides backtrace() and friends
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ARRAY_LENGTH( a ) ( sizeof( a ) / sizeof( ( a )[0] ) )
+
+static FILE *calloc_log_file = NULL;
+
+static int open_log_file( void )
+{
+    if( calloc_log_file == NULL )
+    {
+        const char *filename = getenv( "MBEDTLS_TEST_MEMORY_CALLOC_LOG_FILE" );
+        if( filename == NULL || filename[0] == 0 )
+            filename = "calloc_backtrace.log";
+        calloc_log_file = fopen( filename, "w" );
+    }
+    return( calloc_log_file != NULL );
+}
+
+static void close_log_file( void )
+{
+    if( calloc_log_file != NULL )
+        fclose( calloc_log_file );
+    calloc_log_file = NULL;
+}
+
+static void log_calloc_backtrace( size_t n, size_t size )
+{
+    if( ! open_log_file( ) )
+        return;
+
+    mbedtls_fprintf( calloc_log_file,
+                     "mbedtls_calloc( %zu, %zu )\n",
+                     n, size );
+    fflush( calloc_log_file );
+
+    void *frames[20];
+    size_t frame_count = backtrace( frames, ARRAY_LENGTH( frames ) );
+    backtrace_symbols_fd( frames, frame_count, fileno( calloc_log_file ) );
+    mbedtls_fprintf( calloc_log_file, "\n" );
+}
+#endif /* MBEDTLS_TEST_MEMORY_CALLOC_BACKTRACE */
+
 void *mbedtls_test_calloc_wrapper( size_t n, size_t size )
 {
+#if defined(MBEDTLS_TEST_MEMORY_CALLOC_BACKTRACE)
+    log_calloc_backtrace( n, size );
+#endif
+
     /* Zero-size allocations do not count. This way, if the underlying calloc
      * function returns NULL, we know that the allocation has failed. */
     if( n == 0 || size == 0 )
