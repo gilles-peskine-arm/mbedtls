a97746044d3ba8d641ba6a1388a6e96fde22e58d
Gilles Peskine <Gilles.Peskine@arm.com>
2022-02-25 19:50:38 +0100

    Only run "Default" tests if the ciphersuite is enabled
    These tests ensure that a certain cipher suite is in use, so they fail in
    builds that lack one of the corresponding algorithms.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -1384,6 +1384,7 @@ trap cleanup INT TERM HUP
 # - the expected (highest security) parameters are selected
 #   ("signature_algorithm ext: 6" means SHA-512 (highest common hash))
 requires_config_enabled MBEDTLS_SSL_PROTO_TLS1_2
+requires_ciphersuite_enabled TLS-ECDHE-RSA-WITH-CHACHA20-POLY1305-SHA256
 run_test    "Default" \
             "$P_SRV debug_level=3" \
             "$P_CLI" \
@@ -1396,6 +1397,7 @@ run_test    "Default" \
             -C "error"
 
 requires_config_enabled MBEDTLS_SSL_PROTO_TLS1_2
+requires_ciphersuite_enabled TLS-ECDHE-RSA-WITH-CHACHA20-POLY1305-SHA256
 run_test    "Default, DTLS" \
             "$P_SRV dtls=1" \
             "$P_CLI dtls=1" \
