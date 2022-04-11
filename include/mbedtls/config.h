75a45b5c80aa066a9d7107b6cade46a847c2f70f
Gilles Peskine <Gilles.Peskine@arm.com>
2022-04-11 16:42:37 +0200

    Format literal # in a way that doesn't confuse older Doxygen
    With Doxygen 1.8.11 (as on Ubuntu 16.04), `#include` doesn't protect the
    hash character enough, and Doxygen tries to link to something called
    include. (Doxygen 1.8.17 doesn't have this problem.)
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -3631,9 +3631,9 @@
  * `"mbedtls/mbedtls_config.h"`.
  * This header file specifies the compile-time configuration of Mbed TLS.
  *
- * This macro is expanded after an `#include` directive. This is a popular but
+ * This macro is expanded after an <tt>\#include</tt> directive. This is a popular but
  * non-standard feature of the C language, so this feature is only available
- * with compilers that perform macro expansion on an `#include` line.
+ * with compilers that perform macro expansion on an <tt>\#include</tt> line.
  *
  * The value of this symbol is typically a path in double quotes, relative
  * to a directory on the include search pah.
@@ -3646,9 +3646,9 @@
  * If defined, this is a header which will be included after
  * `"mbedtls/mbedtls_config.h"` or #MBEDTLS_CONFIG_FILE.
  *
- * This macro is expanded after an `#include` directive. This is a popular but
+ * This macro is expanded after an <tt>\#include</tt> directive. This is a popular but
  * non-standard feature of the C language, so this feature is only available
- * with compilers that perform macro expansion on an `#include` line.
+ * with compilers that perform macro expansion on an <tt>\#include</tt> line.
  *
  * The value of this symbol is typically a path in double quotes, relative
  * to a directory on the include search pah.
