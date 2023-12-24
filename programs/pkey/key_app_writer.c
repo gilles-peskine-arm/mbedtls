13ef596330572ac05d1bccc7932fc9898bd816c0
Gilles Peskine <Gilles.Peskine@arm.com>
2023-12-24 15:33:18 +0100

    Restore variable that is RSA-only after all
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- programs/pkey/key_app_writer.c
+++ programs/pkey/key_app_writer.c
@@ -253,9 +253,8 @@ int main(int argc, char *argv[])
     mbedtls_ctr_drbg_context ctr_drbg;
 
     mbedtls_pk_context key;
-    mbedtls_mpi D;
 #if defined(MBEDTLS_RSA_C)
-    mbedtls_mpi N, P, Q, E, DP, DQ, QP;
+    mbedtls_mpi N, P, Q, D, E, DP, DQ, QP;
 #endif /* MBEDTLS_RSA_C */
 
     /*
@@ -278,10 +277,9 @@ int main(int argc, char *argv[])
     }
 #endif /* MBEDTLS_USE_PSA_CRYPTO */
 
-    mbedtls_mpi_init(&D);
 #if defined(MBEDTLS_RSA_C)
     mbedtls_mpi_init(&N); mbedtls_mpi_init(&P); mbedtls_mpi_init(&Q);
-    mbedtls_mpi_init(&E); mbedtls_mpi_init(&DP);
+    mbedtls_mpi_init(&D); mbedtls_mpi_init(&E); mbedtls_mpi_init(&DP);
     mbedtls_mpi_init(&DQ); mbedtls_mpi_init(&QP);
 #endif /* MBEDTLS_RSA_C */
 
@@ -478,10 +476,9 @@ exit:
 #endif
     }
 
-    mbedtls_mpi_free(&D);
 #if defined(MBEDTLS_RSA_C)
     mbedtls_mpi_free(&N); mbedtls_mpi_free(&P); mbedtls_mpi_free(&Q);
-    mbedtls_mpi_free(&E); mbedtls_mpi_free(&DP);
+    mbedtls_mpi_free(&D); mbedtls_mpi_free(&E); mbedtls_mpi_free(&DP);
     mbedtls_mpi_free(&DQ); mbedtls_mpi_free(&QP);
 #endif /* MBEDTLS_RSA_C */
 
