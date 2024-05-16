351f04f5e70f7b95ecbaa9f979688bb8831c5c62
Gilles Peskine <Gilles.Peskine@arm.com>
2024-05-16 14:46:09 +0200

    Error out if *adjust* headers are included manually
    Some projects using Mbed TLS have migrated their configuration
    file (config.h -> mbedtls_config.h, or MBEDTLS_CONFIG_FILE) from Mbed TLS
    2.x, and kept including check_config.h. This is unnecessary since Mbed TLS
    3.0, and increasingly in 3.x it may report spurious errors because the
    configuration adjustments have not been done yet. This has led some
    projects to include configuration adjustment headers manually, but only
    partially or in the wrong order, which can result in silent inconsistencies.
    Error out if this happens, with a message mentioning check_config.h since
    that's the likely root cause.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/mbedtls/config_adjust_psa_superset_legacy.h
+++ include/mbedtls/config_adjust_psa_superset_legacy.h
@@ -19,6 +19,13 @@
 #ifndef MBEDTLS_CONFIG_ADJUST_PSA_SUPERSET_LEGACY_H
 #define MBEDTLS_CONFIG_ADJUST_PSA_SUPERSET_LEGACY_H
 
+#if !defined(MBEDTLS_CONFIG_FILES_READ)
+#error "Do not include mbedtls/config_adjust_*.h manually! This can lead to problems, " \
+       "up to and including runtime errors such as buffer overflows. " \
+       "Since Mbed TLS 3.0, including mbedtls/check_config.h manually is " \
+       "unnecessary and can cause spurious compile-time errors."
+#endif /* !MBEDTLS_CONFIG_FILES_READ */
+
 /****************************************************************/
 /* Hashes that are built in are also enabled in PSA.
  * This simplifies dependency declarations especially
