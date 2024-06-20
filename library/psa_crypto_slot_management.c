7f5b1df0c7953acb221168a3d18ed3d83867cffb
Gilles Peskine <Gilles.Peskine@arm.com>
2024-06-20 22:15:42 +0200

    Assert that key ID ranges don't overlap
    Ensure that a key ID can't be in range for more than one of volatile keys,
    persistent (i.e. user-chosen) keys or built-in keys.
    
    This breaks the build when MBEDTLS_PSA_KEY_SLOT_DYNAMIC is enabled, for good
    reason: there is overlap between the volatile key range and the built-in key
    range. The next commit will fix this.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/psa_crypto_slot_management.c
+++ library/psa_crypto_slot_management.c
@@ -28,6 +28,33 @@
 #endif
 
 
+/* Make sure we have distinct ranges of key identifiers for distinct
+ * purposes. */
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_USER_MIN < PSA_KEY_ID_USER_MAX,
+                      "Empty user key ID range")
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_VENDOR_MIN < PSA_KEY_ID_VENDOR_MAX,
+                      "Empty vendor key ID range")
+MBEDTLS_STATIC_ASSERT(MBEDTLS_PSA_KEY_ID_BUILTIN_MIN < MBEDTLS_PSA_KEY_ID_BUILTIN_MAX,
+                      "Empty builtin key ID range")
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_VOLATILE_MIN < PSA_KEY_ID_VOLATILE_MAX,
+                      "Empty volatile key ID range")
+
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_USER_MAX < PSA_KEY_ID_VENDOR_MIN ||
+                      PSA_KEY_ID_VENDOR_MAX < PSA_KEY_ID_USER_MIN,
+                      "Overlap between user key IDs and vendor key IDs")
+
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_VENDOR_MIN <= MBEDTLS_PSA_KEY_ID_BUILTIN_MIN &&
+                      MBEDTLS_PSA_KEY_ID_BUILTIN_MAX <= PSA_KEY_ID_VENDOR_MAX,
+                      "Builtin key identifiers are not in the vendor range")
+
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_VENDOR_MIN <= PSA_KEY_ID_VOLATILE_MIN &&
+                      PSA_KEY_ID_VOLATILE_MAX <= PSA_KEY_ID_VENDOR_MAX,
+                      "Volatile key identifiers are not in the vendor range")
+
+MBEDTLS_STATIC_ASSERT(PSA_KEY_ID_VOLATILE_MAX < MBEDTLS_PSA_KEY_ID_BUILTIN_MIN ||
+                      MBEDTLS_PSA_KEY_ID_BUILTIN_MAX < PSA_KEY_ID_VOLATILE_MIN,
+                      "Overlap between builtin key IDs and volatile key IDs")
+
 
 #if defined(MBEDTLS_PSA_KEY_SLOT_DYNAMIC)
 
@@ -249,6 +276,9 @@ static inline size_t key_slice_length(size_t slice_idx)
 
 static inline psa_key_slot_t *get_volatile_key_slot(psa_key_id_t key_id)
 {
+    MBEDTLS_STATIC_ASSERT(ARRAY_LENGTH(global_data.key_slots) <=
+                          PSA_KEY_ID_VOLATILE_MAX - PSA_KEY_ID_VOLATILE_MIN + 1,
+                          "The volatile key range is larger than the key slot array");
     return &global_data.key_slots[key_id - PSA_KEY_ID_VOLATILE_MIN];
 }
 
