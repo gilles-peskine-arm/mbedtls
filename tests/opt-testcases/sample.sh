9d54b681b6b28f6d839892c69287f80cf55ab03e
Gilles Peskine <Gilles.Peskine@arm.com>
2024-09-04 16:07:05 +0200

    Test mini_client
    Test mini_client with both TLS 1.2 and TLS 1.3.
    Test against both OpenSSL and GnuTLS.
    
    Add some missing compile-time requirements in mini_client.c: it can use either
    an ECDSA certificate or PSK.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/opt-testcases/sample.sh
+++ tests/opt-testcases/sample.sh
@@ -46,3 +46,43 @@ run_test    "Sample: ssl_client1, gnutls server, TLS 1.3" \
             -c "<TD>Protocol version:</TD><TD>TLS1.3</TD>" \
             -S "Error" \
             -C "error"
+
+requires_protocol_version tls12
+run_test    "Sample: mini_client, openssl server, TLS 1.2" \
+            -P 4433 \
+            "$O_SRV -tls1_2" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "GET / HTTP/1.0" \
+            -S "ERROR" \
+            -C "error"
+
+requires_protocol_version tls12
+run_test    "Sample: mini_client, gnutls server, TLS 1.2" \
+            -P 4433 \
+            "$G_SRV --priority=NORMAL:-VERS-TLS-ALL:+VERS-TLS1.2" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "Version: TLS1.2" \
+            -S "Error" \
+            -C "error"
+
+requires_protocol_version tls13
+run_test    "Sample: mini_client, openssl server, TLS 1.3" \
+            -P 4433 \
+            "$O_SRV -tls1_3" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "GET / HTTP/1.0" \
+            -S "ERROR" \
+            -C "error"
+
+requires_protocol_version tls13
+run_test    "Sample: mini_client, gnutls server, TLS 1.3" \
+            -P 4433 \
+            "$G_SRV --priority=NORMAL:-VERS-TLS-ALL:+VERS-TLS1.3" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "Version: TLS1.3" \
+            -S "Error" \
+            -C "error"
