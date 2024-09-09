38b851ae04007da2c09b54d03fb03f4e3c55bfed
Gilles Peskine <Gilles.Peskine@arm.com>
2024-09-09 21:02:09 +0200

    mini_client: add PSK tests (TLS 1.2 only)
    PSK support in TLS 1.3 would require handling
    MBEDTLS_ERR_SSL_RECEIVED_NEW_SESSION_TICKET.
    
    Test against ssl_server2, openssl s_server and gnutls-serv. Don't test
    against ssl_server, because their application protocols aren't compatible
    enough.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/opt-testcases/sample.sh
+++ tests/opt-testcases/sample.sh
@@ -132,6 +132,41 @@ run_test    "Sample: dtls_client, gnutls server, DTLS 1.2" \
             -S "Error" \
             -C "error"
 
+run_test    "Sample: mini_client, ssl_server2, TLS 1.2 PSK" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server2 force_ciphersuite=TLS-PSK-WITH-AES-128-CCM \
+                                       psk_identity=Client_identity psk=000102030405060708090a0b0c0d0e0f" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
+requires_protocol_version tls12
+run_test    "Sample: mini_client, openssl server, TLS 1.2 PSK" \
+            -P 4433 \
+            "$O_SRV -tls1_2 -allow_no_dhe_kex -nocert \
+                    -psk_identity Client_identity -psk 000102030405060708090a0b0c0d0e0f" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "GET / HTTP/1.0" \
+            -S "ERROR" \
+            -C "error"
+
+requires_protocol_version tls12
+run_test    "Sample: mini_client, gnutls server, TLS 1.2 PSK" \
+            -P 4433 \
+            "$G_SRV --priority=NORMAL:+AES-128-CCM-8:+AES-256-CCM-8:-VERS-TLS-ALL:+VERS-TLS1.2:-KX-ALL:+PSK \
+                    --pskpasswd=$PROGRAMS_DIR/mini_client.psk" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "Version: TLS1.2" \
+            -S "Error" \
+            -C "error"
+
+# No TLS 1.3 PSK test cases for mini_client: it isn't supported.
+
 requires_certificate_authentication
 run_test    "Sample: mini_client, ssl_server2, certificate" \
             -P 4433 \
@@ -144,7 +179,8 @@ run_test    "Sample: mini_client, ssl_server2, certificate" \
             -C "error"
 
 requires_protocol_version tls12
-run_test    "Sample: mini_client, openssl server, TLS 1.2" \
+requires_certificate_authentication
+run_test    "Sample: mini_client, openssl server, TLS 1.2 certificate" \
             -P 4433 \
             "$O_SRV -tls1_2" \
             "$PROGRAMS_DIR/mini_client" \
@@ -154,7 +190,8 @@ run_test    "Sample: mini_client, openssl server, TLS 1.2" \
             -C "error"
 
 requires_protocol_version tls12
-run_test    "Sample: mini_client, gnutls server, TLS 1.2" \
+requires_certificate_authentication
+run_test    "Sample: mini_client, gnutls server, TLS 1.2 certificate" \
             -P 4433 \
             "$G_SRV --priority=NORMAL:-VERS-TLS-ALL:+VERS-TLS1.2" \
             "$PROGRAMS_DIR/mini_client" \
@@ -166,7 +203,8 @@ run_test    "Sample: mini_client, gnutls server, TLS 1.2" \
 requires_protocol_version tls13
 requires_openssl_tls1_3
 requires_config_enabled MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE
-run_test    "Sample: mini_client, openssl server, TLS 1.3" \
+requires_certificate_authentication
+run_test    "Sample: mini_client, openssl server, TLS 1.3 certificate" \
             -P 4433 \
             "$O_NEXT_SRV -tls1_3" \
             "$PROGRAMS_DIR/mini_client" \
@@ -178,7 +216,8 @@ run_test    "Sample: mini_client, openssl server, TLS 1.3" \
 requires_protocol_version tls13
 requires_gnutls_tls1_3
 requires_config_enabled MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE
-run_test    "Sample: mini_client, gnutls server, TLS 1.3" \
+requires_certificate_authentication
+run_test    "Sample: mini_client, gnutls server, TLS 1.3 certificate" \
             -P 4433 \
             "$G_NEXT_SRV --priority=NORMAL:-VERS-TLS-ALL:+VERS-TLS1.3" \
             "$PROGRAMS_DIR/mini_client" \
@@ -210,6 +249,10 @@ run_test    "Sample: ssl_client1 with ssl_server" \
             -c "[1-9][0-9]* bytes written" \
             -S "error" \
             -C "error"
+
+# We don't test mini_client against ssl_server because their
+# application protocols are too incompatible, which causes the client
+# to error out.
 
 requires_protocol_version tls12
 run_test    "Sample: ssl_server, openssl client, TLS 1.2" \
