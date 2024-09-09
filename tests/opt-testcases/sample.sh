32ecece366f5dcf33678c33489358951b5d23b56
Gilles Peskine <Gilles.Peskine@arm.com>
2024-09-10 00:03:18 +0200

    Test SSL sample programs against each other and ssl_client2, ssl_server2
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/opt-testcases/sample.sh
+++ tests/opt-testcases/sample.sh
@@ -1,9 +1,22 @@
-# Test that SSL sample programs can interoperate with OpenSSL and GnuTLS.
+# Test that SSL sample programs can interoperate with each other
+# and with OpenSSL and GnuTLS.
 
 # Copyright The Mbed TLS Contributors
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 
 : ${PROGRAMS_DIR:=../programs/ssl}
+
+run_test    "Sample: ssl_client1, ssl_server2" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server2" \
+            "$PROGRAMS_DIR/ssl_client1" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
 
 requires_protocol_version tls12
 run_test    "Sample: ssl_client1, openssl server, TLS 1.2" \
@@ -76,6 +89,22 @@ run_test    "Sample: ssl_client1, gnutls server, TLS 1.3, no middlebox compatibi
             -S "Error" \
             -C "error"
 
+# The server complains of extra data after it closes the connection
+# because the client keeps sending data, so the server receives
+# more application data when it expects a new handshake. We consider
+# the test a success if both sides have sent and received application
+# data, no matter what happens afterwards.
+run_test    "Sample: dtls_client, ssl_server2" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server2 dtls=1 server_addr=localhost" \
+            "$PROGRAMS_DIR/dtls_client" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -C "error"
+
 requires_protocol_version dtls12
 run_test    "Sample: dtls_client, openssl server, DTLS 1.2" \
             -P 4433 \
@@ -103,6 +132,16 @@ run_test    "Sample: dtls_client, gnutls server, DTLS 1.2" \
             -S "Error" \
             -C "error"
 
+run_test    "Sample: mini_client, ssl_server2, PSK" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server2 psk_identity=Client_identity psk=000102030405060708090a0b0c0d0e0f" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
 requires_protocol_version tls12
 run_test    "Sample: mini_client, openssl server, TLS 1.2 PSK" \
             -P 4433 \
@@ -126,6 +165,21 @@ run_test    "Sample: mini_client, gnutls server, TLS 1.2 PSK" \
             -C "error"
 
 # No TLS 1.3 PSK test cases for mini_client: it isn't supported.
+
+requires_certificate_authentication
+run_test    "Sample: mini_client, ssl_server2, certificate" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server2" \
+            "$PROGRAMS_DIR/mini_client" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
+# We don't test mini_client against ssl_server because their
+# application protocol is too incompatible, which causes the client
+# to error out.
 
 requires_protocol_version tls12
 requires_certificate_authentication
@@ -171,6 +225,30 @@ run_test    "Sample: mini_client, gnutls server, TLS 1.3 certificate" \
             0 \
             -s "Version: TLS1.3" \
             -S "Error" \
+            -C "error"
+
+run_test    "Sample: ssl_server, ssl_client2" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server" \
+            "$PROGRAMS_DIR/ssl_client2" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
+run_test    "Sample: ssl_client1 with ssl_server" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_server" \
+            "$PROGRAMS_DIR/ssl_client1" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
             -C "error"
 
 requires_protocol_version tls12
@@ -251,6 +329,30 @@ run_test    "Sample: ssl_server, gnutls client, TLS 1.3, no middlebox compatibil
             -S "error" \
             -C "ERROR"
 
+run_test    "Sample: ssl_fork_server, ssl_client2" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_fork_server" \
+            "$PROGRAMS_DIR/ssl_client2" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
+run_test    "Sample: ssl_client1 with ssl_fork_server" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_fork_server" \
+            "$PROGRAMS_DIR/ssl_client1" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
 requires_protocol_version tls12
 run_test    "Sample: ssl_fork_server, openssl client, TLS 1.2" \
             -P 4433 \
@@ -301,6 +403,30 @@ run_test    "Sample: ssl_fork_server, gnutls client, TLS 1.3" \
             -S "error" \
             -C "ERROR"
 
+run_test    "Sample: ssl_pthread_server, ssl_client2" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_pthread_server" \
+            "$PROGRAMS_DIR/ssl_client2" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
+run_test    "Sample: ssl_client1 with ssl_pthread_server" \
+            -P 4433 \
+            "$PROGRAMS_DIR/ssl_pthread_server" \
+            "$PROGRAMS_DIR/ssl_client1" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -S "error" \
+            -C "error"
+
 requires_protocol_version tls12
 run_test    "Sample: ssl_pthread_server, openssl client, TLS 1.2" \
             -P 4433 \
@@ -350,6 +476,38 @@ run_test    "Sample: ssl_pthread_server, gnutls client, TLS 1.3" \
             -c "Description:.*TLS1.3" \
             -S "error" \
             -C "ERROR"
+
+# The server complains of extra data after it closes the connection
+# because the client keeps sending data, so the server receives
+# more application data when it expects a new handshake. We consider
+# the test a success if both sides have sent and received application
+# data, no matter what happens afterwards.
+run_test    "Sample: dtls_client with dtls_server" \
+            -P 4433 \
+            "$PROGRAMS_DIR/dtls_server" \
+            "$PROGRAMS_DIR/dtls_client" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -C "error"
+
+# The server complains of extra data after it closes the connection
+# because the client keeps sending data, so the server receives
+# more application data when it expects a new handshake. We consider
+# the test a success if both sides have sent and received application
+# data, no matter what happens afterwards.
+run_test    "Sample: ssl_client2, dtls_server" \
+            -P 4433 \
+            "$PROGRAMS_DIR/dtls_server" \
+            "$PROGRAMS_DIR/ssl_client2 dtls=1" \
+            0 \
+            -s "[1-9][0-9]* bytes read" \
+            -s "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -c "[1-9][0-9]* bytes written" \
+            -C "error"
 
 requires_protocol_version dtls12
 run_test    "Sample: dtls_server, openssl client, DTLS 1.2" \
