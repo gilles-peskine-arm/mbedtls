cb0177846884601d64aec9b50b1686cff746a7d6
Gilles Peskine <Gilles.Peskine@arm.com>
2024-11-07 15:04:08 +0100

    Create header files for context type definitions
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ tf-psa-crypto/drivers/builtin/include/mbedtls/opaque/hash_contexts.h
@@ -0,0 +1,33 @@
+/**
+ * \file hash_contexts.h
+ *
+ * \brief Operation contexts for hashes.
+ *
+ * \note The contents of this file are not part of the stable interface of
+ *       the library. This file defines the implementation of structure
+ *       types. These types are not part of the API, but they are part
+ *       of the ABI, so the library SO version must be incremented when
+ *       the types change.
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+#ifndef MBEDTLS_OPAQUE_HASH_CONTEXTS_H
+#define MBEDTLS_OPAQUE_HASH_CONTEXTS_H
+#include "mbedtls/private_access.h"
+#include "mbedtls/build_info.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* hash_contexts.h */
