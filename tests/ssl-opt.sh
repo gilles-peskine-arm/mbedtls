92956303473248f0120d532df4174eb48f7a1a19
Gilles Peskine <Gilles.Peskine@arm.com>
2025-03-03 10:34:26 +0100

    $OPENSSL_NEXT s_client: allow pure-PSK by default
    Tell the OpenSSL client to allow pure-PSK key exchange modes in TLS 1.3
    (only OPENSSL_NEXT, the option only exists since OpeNSSL 1.1.1). This fixes
    test cases that use OPENSSL_NEXT in TLS 1.3 either by explicitly arranging a
    pure-PSK mode, or in pure-PSK builds.
    
    In some test cases in tls13-kex-modes.sh, we rely on OpenSSL not offering
    pure-PSK modes. In those test cases, keep pure-PSK disabled.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -98,17 +98,21 @@ if [ -z "${GNUTLS_NEXT_SERV:-}" ]; then
 fi
 
 if [ -n "${OPENSSL_NEXT:-}" ]; then
-    O_NEXT_SRV="$OPENSSL_NEXT s_server -www -cert $DATA_FILES_PATH/server5.crt -key $DATA_FILES_PATH/server5.key"
-    O_NEXT_SRV_EARLY_DATA="$OPENSSL_NEXT s_server -early_data -cert $DATA_FILES_PATH/server5.crt -key $DATA_FILES_PATH/server5.key"
-    O_NEXT_SRV_NO_CERT="$OPENSSL_NEXT s_server -www "
-    O_NEXT_CLI="echo 'GET / HTTP/1.0' | $OPENSSL_NEXT s_client -CAfile $DATA_FILES_PATH/test-ca_cat12.crt"
-    O_NEXT_CLI_NO_CERT="echo 'GET / HTTP/1.0' | $OPENSSL_NEXT s_client"
+    O_NEXT_SRV_NO_CERT="$OPENSSL_NEXT s_server -www -allow_no_dhe_kex"
+    O_NEXT_SRV="$O_NEXT_SRV_NO_CERT -cert $DATA_FILES_PATH/server5.crt -key $DATA_FILES_PATH/server5.key"
+    O_NEXT_SRV_EARLY_DATA="$O_NEXT_SRV -early_data"
+    O_NEXT_CLI_BASE="echo 'GET / HTTP/1.0' | $OPENSSL_NEXT s_client"
+    O_NEXT_CLI_NO_CERT="$O_NEXT_CLI_BASE -allow_no_dhe_kex"
+    O_NEXT_CLI="$O_NEXT_CLI_NO_CERT -CAfile $DATA_FILES_PATH/test-ca_cat12.crt"
+    O_NEXT_CLI_NO_PURE_PSK="$O_NEXT_CLI_BASE -CAfile $DATA_FILES_PATH/test-ca_cat12.crt"
 else
     O_NEXT_SRV=false
     O_NEXT_SRV_NO_CERT=false
     O_NEXT_SRV_EARLY_DATA=false
+    O_NEXT_CLI_BASE=false
     O_NEXT_CLI_NO_CERT=false
     O_NEXT_CLI=false
+    O_NEXT_CLI_NO_PURE_PSK=false
 fi
 
 if [ -n "${GNUTLS_NEXT_SERV:-}" ]; then
@@ -2137,8 +2141,10 @@ if [ "$LIST_TESTS" -eq 0 ];then
         O_NEXT_SRV="$O_NEXT_SRV -accept $SRV_PORT"
         O_NEXT_SRV_NO_CERT="$O_NEXT_SRV_NO_CERT -accept $SRV_PORT"
         O_NEXT_SRV_EARLY_DATA="$O_NEXT_SRV_EARLY_DATA -accept $SRV_PORT"
+        O_NEXT_CLI_BASE="$O_NEXT_CLI_BASE -connect 127.0.0.1:+SRV_PORT"
         O_NEXT_CLI="$O_NEXT_CLI -connect 127.0.0.1:+SRV_PORT"
         O_NEXT_CLI_NO_CERT="$O_NEXT_CLI_NO_CERT -connect 127.0.0.1:+SRV_PORT"
+        O_NEXT_CLI_NO_PURE_PSK="$O_NEXT_CLI_NO_PURE_PSK -connect 127.0.0.1:+SRV_PORT"
     fi
 
     if [ -n "${GNUTLS_NEXT_SERV:-}" ]; then
