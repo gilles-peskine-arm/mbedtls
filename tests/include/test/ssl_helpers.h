934b857e6aa7a1a08882bee36877e001553f77ac
Gilles Peskine <Gilles.Peskine@arm.com>
2025-05-27 13:27:22 +0200

    Refactor set_ciphersuites to work on the endpoint structure
    Link the ciphersuite list that's passed to mbedtls_ssl_conf_ciphersuites(),
    and needs to survive in memory as long as the configuration object is live,
    in the endpoint structure. This way it doesn't have to be a local variable
    in mbedtls_test_ssl_do_handshake_with_endpoints().
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/include/test/ssl_helpers.h
+++ tests/include/test/ssl_helpers.h
@@ -196,6 +196,7 @@ typedef struct mbedtls_test_ssl_endpoint {
     mbedtls_test_mock_socket socket;
 
     /* Objects owned by the endpoint */
+    int *ciphersuites;
     mbedtls_x509_crt *ca_chain;
     mbedtls_x509_crt *cert;
     mbedtls_pk_context *pkey;
