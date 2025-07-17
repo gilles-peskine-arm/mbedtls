gilles-peskine-arm/TF-PSA-Crypto

Unit tests for checks for removed options in the config file
Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

tests/scripts/components-basic-checks.sh
@@ -138,6 +138,9 @@ component_tf_psa_crypto_check_test_helpers () {
 
     msg "unit test: translate_ciphers.py"
     python3 -m unittest framework/scripts/translate_ciphers.py 2>&1
+
+    msg "unit test: generate_config_checks.py"
+    tests/scripts/test_generate_config_checks.py
 }
 
 component_tf_psa_crypto_check_names () {

gilles-peskine-arm/mbedtls

tests/scripts/components-basic-checks.sh
@@ -123,4 +123,7 @@ component_check_test_helpers () {
 
     msg "unit test: translate_ciphers.py"
     python3 -m unittest framework/scripts/translate_ciphers.py 2>&1
+
+    msg "unit test: generate_config_checks.py"
+    tests/scripts/test_config_checks.py
 }