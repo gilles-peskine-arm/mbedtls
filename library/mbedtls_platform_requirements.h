f3dd7eba12f7c8b4566ee480166b7eed32a7c485
Gilles Peskine <Gilles.Peskine@arm.com>
2026-01-30 18:06:56 +0100

    Start from a clean baseline for C11 ext1 and POSIX features
    Define `_POSIX_C_SOURCE` and `_XOPEN_SOURCE` in a single place that
    applies everywhere, to make things simple.
    
    This may break some platforms that require special handling for POSIX
    functions and types. Subsequent commits will add platform-specific hacks
    as needed.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/mbedtls_platform_requirements.h
+++ library/mbedtls_platform_requirements.h
@@ -15,4 +15,18 @@
 #ifndef MBEDTLS_MBEDTLS_PLATFORM_REQUIREMENTS_H
 #define MBEDTLS_MBEDTLS_PLATFORM_REQUIREMENTS_H
 
+#if !defined(_POSIX_C_SOURCE)
+/* For standards-compliant access to
+ * getaddrinfo(),
+ * ... */
+#define _POSIX_C_SOURCE 200112L
+#endif
+
+#if !defined(_XOPEN_SOURCE)
+/* For standards-compliant access to
+ * sockaddr_storage,
+ * ... */
+#define _XOPEN_SOURCE 600
+#endif
+
 #endif /* MBEDTLS_MBEDTLS_PLATFORM_REQUIREMENTS_H */
