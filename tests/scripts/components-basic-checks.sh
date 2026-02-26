bebd675d60344fafb2180ebaa372499cd9f39011
Gilles Peskine <Gilles.Peskine@arm.com>
2026-02-26 12:51:24 +0100

    Move check_committed_generated_files to its own component
    This will probably help when a framework change causes the content of these
    files to change. See https://github.com/Mbed-TLS/mbedtls-test/issues/252
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/components-basic-checks.sh
+++ tests/scripts/components-basic-checks.sh
@@ -16,16 +16,6 @@ component_check_recursion () {
     ./framework/scripts/recursion.pl ${BUILTIN_SRC_PATH}/*.c
 }
 
-support_check_generated_files () {
-    # Add requirements on the Python installation here for
-    # the sake of check_committed_generated_files.py in mbedtls.
-    #
-    # Check the Python version, not the presence of the package,
-    # because the CI runs `all.sh --list-components` outside of the
-    # venv that has our desired packages.
-    :
-}
-
 component_check_generated_files () {
     msg "Check make_generated_files.py consistency"
     $MAKE_COMMAND neat
@@ -57,7 +47,19 @@ component_check_generated_files () {
 
     # This component ends with the generated files present in the source tree.
     # This is necessary for subsequent components!
+}
 
+support_check_committed_generated_files () {
+    # Add requirements on the Python installation here for
+    # the sake of check_committed_generated_files.py in mbedtls.
+    #
+    # Check the Python version, not the presence of the package,
+    # because the CI runs `all.sh --list-components` outside of the
+    # venv that has our desired packages.
+    :
+}
+
+component_check_committed_generated_files () {
     msg "Check committed generated files"
     tests/scripts/check_committed_generated_files.py
 }
